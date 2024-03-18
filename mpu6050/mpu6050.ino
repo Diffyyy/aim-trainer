92e95af2ea1558322d85b9bc4a3a3bb9321b8a0c
alyssaysabelle <85222862+alyssaysabelle@users.noreply.github.com>
2024-03-18 16:50:40 +0800

    update gyro value

--- mpu6050/mpu6050.ino
+++ mpu6050/mpu6050.ino
@@ -12,12 +12,12 @@ void setup() {
 void loop() {
  int16_t gx, gy, gz;
  mpu.getRotation(&gx, &gy, &gz);
- float x = gx / 1000;
- float y = gy / 1000;
- float z = gz / 1000;
+ float x = gx / 400;
+ float y = gy / 400;
+ float z = gz / 400;
  Serial.print("X: "); Serial.print(x);
  Serial.print(" Y: "); Serial.print(y);
  Serial.print(" Z: "); Serial.println(z);
 
- delay(1000);
+ delay(100);
 }
